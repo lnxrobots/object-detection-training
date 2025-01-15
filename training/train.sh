a8b643e45e3969f4d778e54883d568c26c6e84d5
Matus Misiak <mato.mat111@gmail.com>
2025-01-15 16:33:25 +0100

    Fix export path and add zipping of detection results in training script

--- training/train.sh
+++ training/train.sh
@@ -20,6 +20,8 @@ cd /workspace/ultralytics
 
 yolo detect train data=datasets/$1/data.yaml pretrained=0 epochs=200 mosaic=0 translate=0 degrees=0 scale=0 shear=0 perspective=0 batch=16
 
-last_folder=$(ls runs/ | sort | tail -n 1)
+last_folder=$(ls runs/detect/ | sort | tail -n 1)
 
-yolo export model=last_folder/weights/best.pt imgsz=640 format=onnx opset=11
+cd runs/detect/$last_folder
+yolo export model=weights/best.pt imgsz=640 format=onnx opset=11
+zip -r /workspace/ultralytics/runs/$1.zip *
